0b18b67a62d656da4d71cb4a4bdc3fb2af85f5f7
vgaddavcg <vgadda@meetveracity.com>
2017-05-23 12:55:59 -0400

    updated for swagger and testing url in swagger with JWT token

--- ascent-demo-service/src/main/java/gov/va/ascent/demo/service/AscentDemoServiceApplication.java
+++ ascent-demo-service/src/main/java/gov/va/ascent/demo/service/AscentDemoServiceApplication.java
@@ -11,11 +11,29 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * An <tt>Ascent Demo Service Application</tt> enabled for Spring Boot Application, 
@@ -32,7 +50,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableCaching
 @EnableAsync
 @EnableAscentSecurity
-@Import(AscentDemoServiceConfig.class) 
+@Import(AscentDemoServiceConfig.class)
 public class AscentDemoServiceApplication extends SpringBootServletInitializer {
 
     @Override
@@ -48,5 +66,47 @@ public class AscentDemoServiceApplication extends SpringBootServletInitializer {
     AlwaysSampler alwaysSampler() {
         return new AlwaysSampler();
     }
- 
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "AUTHORIZATION", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/demo.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+
+        List<SecurityReference> list = new ArrayList<>();
+        list.add(new SecurityReference("Authorization", authorizationScopes));
+        return list;
+    }
+
+    @Bean
+    SecurityConfiguration security() {
+        return new SecurityConfiguration(
+                null,
+                null,
+                null,
+                null,
+                "BEARER ",
+                ApiKeyVehicle.HEADER,
+                "AUTHORIZATION",
+                null);
+    }
 }
