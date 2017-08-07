6a09deb9051edb06e5fe7c0fb7dabbf492cbaa38
vgaddavcg <vgadda@meetveracity.com>
2017-08-07 11:55:29 -0400

    Fix the conflicts
    # Conflicts:
    #	ascent-demo-service/src/main/java/gov/va/ascent/demo/service/config/AscentDemoServiceConfig.java
    #	ascent-demo-service/src/main/java/gov/va/ascent/demo/service/rest/client/DemoServiceRestClientTests.java
    #	ascent-demo-service/src/main/java/gov/va/ascent/demo/service/rest/provider/DemoServiceEndpoint.java

--- ascent-demo-service/src/main/java/gov/va/ascent/demo/service/config/AscentDemoServiceConfig.java
+++ ascent-demo-service/src/main/java/gov/va/ascent/demo/service/config/AscentDemoServiceConfig.java
@@ -1,6 +1,7 @@
 package gov.va.ascent.demo.service.config;
 
 import gov.va.ascent.demo.partner.person.ws.client.PersonWsClientConfig;
+import gov.va.ascent.demo.service.cache.DemoCacheConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
