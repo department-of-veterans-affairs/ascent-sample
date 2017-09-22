c68b92d71a10ed3594f6efcb2e9b8f12d2827b74
prashanthmds <prashanthmds@gmail.com>
2017-09-22 17:52:34 +0200

    Cloning the ascent-platform repo

--- ascent-developer-environment-vm-setup/cloneascentrepo.sh
+++ ascent-developer-environment-vm-setup/cloneascentrepo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo '######## Starting to clone the Ascent project repository ########'
+echo '######## Starting to clone the Ascent-Platform project repository ########'
 mkdir -p projects/workspace
 cd projects/workspace
-git clone git@github.com:department-of-veterans-affairs/ascent.git
-echo '######## Ascent project repository cloned under /home/vagrant/projects/workspace ########' 
+git clone git@github.com:department-of-veterans-affairs/ascent-platform.git
+echo '######## Ascent-Platform project repository cloned under /home/vagrant/projects/workspace ########' 
