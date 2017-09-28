a1809881435e706583d31ade81971eb25c4661c3
prashanthmds <prashanthmds@gmail.com>
2017-09-28 03:50:57 +0200

    Removed the ssh key setup for github connectivity, instead use https connection

--- ascent-developer-environment-vm-setup/cloneascentrepo.sh
+++ ascent-developer-environment-vm-setup/cloneascentrepo.sh
@@ -2,5 +2,5 @@
 echo '######## Starting to clone the Ascent-Platform project repository ########'
 mkdir -p projects/workspace
 cd projects/workspace
-git clone git@github.com:department-of-veterans-affairs/ascent-platform.git
+git clone https://github.com/department-of-veterans-affairs/ascent-platform.git
 echo '######## Ascent-Platform project repository cloned under /home/vagrant/projects/workspace ########' 
