43a4572bbc2a9b12f642f4106859fd2e72240202
prashanthmds <prashanthmds@gmail.com>
2017-09-28 03:50:57 +0200

    Removed the ssh key setup for github connectivity, instead use https connection

--- ascent-developer-environment-vm-setup/cloneascentsamplerepo.sh
+++ ascent-developer-environment-vm-setup/cloneascentsamplerepo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo '######## Starting to clone the ascent-sample project repository ########'
 cd projects/workspace
-git clone git@github.com:department-of-veterans-affairs/ascent-sample.git
+git clone https://github.com/department-of-veterans-affairs/ascent-sample.git
 echo '######## ascent-sample project repository cloned under /home/vagrant/projects/workspace ########' 
--- ascent-developer-environment-vm-setup/vm-base.sh
+++ ascent-developer-environment-vm-setup/vm-base.sh
@@ -11,11 +11,11 @@ sudo apt-get -y --force-yes --no-install-recommends install xubuntu-desktop mous
 xfce4-goodies xubuntu-wallpapers gksu cifs-utils xfce4-whiskermenu-plugin firefox \
 xarchiver filezilla
 
-echo 'Set italian timezone...'
+echo 'Set US/EST timezone...'
 echo "------------------------"
-echo "Europe/Rome" | sudo tee /etc/timezone
+echo "America/New_York" | sudo tee /etc/timezone
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
-echo 'Set italian keyboard layout...'
+echo 'Set US keyboard layout...'
 echo "------------------------"
-sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="it"/g' /etc/default/keyboard
+sudo sed -i 's/XKBLAYOUT="us"/XKBLAYOUT="us"/g' /etc/default/keyboard
