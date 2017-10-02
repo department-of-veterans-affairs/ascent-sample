5b23c1abd0e272c618d7d1f7a80edb72db070eb1
prashanthmds <prashanthmds@gmail.com>
2017-10-02 18:12:13 +0200

    Removed the VM setup scripts from ascent-sample repo

--- ascent-developer-environment-vm-setup/cloneascentsamplerepo.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo '######## Starting to clone the ascent-sample project repository ########'
-cd projects/workspace
-git clone https://github.com/department-of-veterans-affairs/ascent-sample.git
-echo '######## ascent-sample project repository cloned under /home/vagrant/projects/workspace ########' 
--- ascent-developer-environment-vm-setup/setupsshkey-github.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo '######## Setting up ssh  key ########'
-cd
-ssh-keygen -t rsa -b 4096 -C "My GitHub Account"
-echo '######## ssh key generated ########'
-eval 'ssh-agent -s'
-eval $(ssh-agent -s)
-ssh-add -k ~/.ssh/id_rsa
-echo '######## ssh key added to ssh-agent ########'
