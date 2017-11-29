c284efaf5fffd99174c559225e7629c626ec6111
suchitharavi <sravi@ip-192-168-47-70.us-gov-west-1.compute.internal>
2017-11-29 09:02:20 -0500

    Changed the property file name.

--- ascent-demo-service-inttest/src/test/java/gov/va/ascent/steps/DemoSearchSSNSteps.java
+++ ascent-demo-service-inttest/src/test/java/gov/va/ascent/steps/DemoSearchSSNSteps.java
@@ -13,7 +13,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import gov.va.ascent.util.BaseStepDef;
-import gov.va.ascent.util.RESTConfig;
 
 public class DemoSearchSSNSteps extends BaseStepDef {
 
