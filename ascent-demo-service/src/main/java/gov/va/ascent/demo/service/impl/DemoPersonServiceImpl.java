80628f25a8a6adfc473247624b82fc80440f46fc
Abhijit Kulkarni <akulkarni@meetveracity.com>
2017-12-08 12:12:42 -0500

    Updated Demo Service Impl to show fallback exception implementation

--- ascent-demo-service/src/main/java/gov/va/ascent/demo/service/impl/DemoPersonServiceImpl.java
+++ ascent-demo-service/src/main/java/gov/va/ascent/demo/service/impl/DemoPersonServiceImpl.java
@@ -1,5 +1,8 @@
 package gov.va.ascent.demo.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.JAXBElement;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
@@ -31,6 +34,7 @@ import gov.va.ascent.demo.service.api.DemoPersonService;
 import gov.va.ascent.demo.service.utils.HystrixCommandConstants;
 import gov.va.ascent.demo.service.utils.StringUtil;
 import gov.va.ascent.framework.exception.AscentRuntimeException;
+import gov.va.ascent.framework.messages.Message;
 import gov.va.ascent.framework.messages.MessageSeverity;
 import gov.va.ascent.framework.util.Defense;
 
@@ -80,94 +84,150 @@ public class DemoPersonServiceImpl implements DemoPersonService {
 	protected static final ObjectFactory PERSON_OBJECT_FACTORY = new ObjectFactory();
 
 	/* (non-Javadoc)
-	 * @see gov.va.ascent.demo.service.api.DemoPersonService#getPersonInfo
-	 *         (gov.va.ascent.demo.partner.person.ws.client.transfer.PersonInfoRequest)
-	 * 
-	 * @CachePut Annotation In contrast to the {@link Cacheable @Cacheable} annotation, this annotation does not 
-	 * cause the advised method to be skipped. Rather, it always causes the method to be invoked and its result to be stored in the 
-	 * associated cache
-	 * 
-	 */
-	@Override
-	@CachePut(value="demoPersonService", key="#personInfoRequest", unless="#result == null")
-	@HystrixCommand(
-			fallbackMethod = "getPersonInfoFallBack", 
-			commandKey = "GetPersonInfoBySSNCommand", 
-			ignoreExceptions = {IllegalArgumentException.class})
-	public PersonInfoResponse getPersonInfo(PersonInfoRequest personInfoRequest) {
-		// Check for valid input arguments and WS Client reference.
-		Defense.notNull(personWsClient, "Unable to proceed with Person Service request. The personWsClient must not be null.");
-		Defense.notNull(personInfoRequest, "Invalid argument, personInfoRequest must not be null.");
-		Defense.notNull(personInfoRequest.getSsn(), "Invalid personInfoRequest. SSN must not be null.");
-		Defense.isTrue(personInfoRequest.getSsn().length() == SSN_LENGTH, "Invalid personInfoRequest SSN. Length must be "
-				+ SSN_LENGTH);
-		// Prepare the WS request
-		final JAXBElement<FindPersonBySSN> findPersonBySSNRequestElement = createFindPersonBySSNRequest(personInfoRequest);
-		
-		LOGGER.debug("FindPersonBySSN JAXBElement: {}", 
-				(findPersonBySSNRequestElement != null ? ReflectionToStringBuilder.toString(findPersonBySSNRequestElement): null));
+     * @see gov.va.ascent.demo.service.api.DemoPersonService#getPersonInfo
+     *         (gov.va.ascent.demo.partner.person.ws.client.transfer.PersonInfoRequest)
+     * 
+     * @CachePut Annotation In contrast to the {@link Cacheable @Cacheable} annotation, this annotation does not 
+     * cause the advised method to be skipped. Rather, it always causes the method to be invoked and its result to be stored in the 
+     * associated cache
+     * 
+     */
+    @Override
+    @CachePut(value="demoPersonService", key="#personInfoRequest", unless="#result == null")
+    @HystrixCommand(
+            fallbackMethod = "getPersonInfoFallBack", 
+            commandKey = "GetPersonInfoBySSNCommand", 
+            ignoreExceptions = {IllegalArgumentException.class})
+    public PersonInfoResponse getPersonInfo(PersonInfoRequest personInfoRequest) {
+        // Check for valid input arguments and WS Client reference.
+        Defense.notNull(personWsClient, "Unable to proceed with Person Service request. The personWsClient must not be null.");
+        Defense.notNull(personInfoRequest, "Invalid argument, personInfoRequest must not be null.");
+        Defense.notNull(personInfoRequest.getSsn(), "Invalid personInfoRequest. SSN must not be null.");
+        Defense.isTrue(personInfoRequest.getSsn().length() == SSN_LENGTH, "Invalid personInfoRequest SSN. Length must be "
+                + SSN_LENGTH);
+        if (cacheManager.getCache("demoPersonService") != null && cacheManager.getCache("demoPersonService").get(personInfoRequest) != null) {
+          LOGGER.info("returning cached data for {}", personInfoRequest);
+          return cacheManager.getCache("demoPersonService").get(personInfoRequest, PersonInfoResponse.class);
+        } else {
+            // Prepare the WS request
+            final JAXBElement<FindPersonBySSN> findPersonBySSNRequestElement = createFindPersonBySSNRequest(personInfoRequest);
+            
+            LOGGER.debug("FindPersonBySSN JAXBElement: {}", 
+                    (findPersonBySSNRequestElement != null ? ReflectionToStringBuilder.toString(findPersonBySSNRequestElement): null));
+    
+            // Invoke the Person Web Service via the WS Client
+            final JAXBElement<FindPersonBySSNResponse> findPersonBySSNResponseElement = personWsClient.getPersonInfo(findPersonBySSNRequestElement);
+            
+            LOGGER.debug("FindPersonBySSNResponse JAXBElement: {}", 
+                    (findPersonBySSNResponseElement != null ? ReflectionToStringBuilder.toString(findPersonBySSNResponseElement): null));
+    
+            // Prepare the service response
+            final PersonInfoResponse personInfoResponse =
+                    createPersonInfoResponse(findPersonBySSNResponseElement, personInfoRequest.getSsn());
+            LOGGER.debug("PersonInfoResponse: {}", 
+                    ReflectionToStringBuilder.toString(personInfoResponse));
+            return personInfoResponse;
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see gov.va.ascent.demo.service.api.DemoPersonService#findPersonByParticipantID
+     *                 (gov.va.ascent.demo.partner.person.ws.client.transfer.PersonInfoRequest)
+     * @Cacheable Annotation indicating that the result of invoking a method (or all methods in a class) can be cached.
+     */
+    @Override
+    @CachePut(value="demoPersonService", key="#personInfoRequest", unless="#result == null")
+    @HystrixCommand(
+                fallbackMethod = "findPersonByParticipantIDFallBack", 
+                commandKey = "GetPersonInfoByPIDCommand",
+                ignoreExceptions = {IllegalArgumentException.class})
+    public PersonInfoResponse findPersonByParticipantID(final PersonInfoRequest personInfoRequest) {
+        
+        // Check for valid input arguments and WS Client reference.
+        Defense.notNull(personWsClient, "Unable to proceed with Person Service request. The personWsClient must not be null.");
+        Defense.notNull(personInfoRequest.getParticipantID(), "Invalid argument, pid must not be null.");
 
-		// Invoke the Person Web Service via the WS Client
-		final JAXBElement<FindPersonBySSNResponse> findPersonBySSNResponseElement = personWsClient.getPersonInfo(findPersonBySSNRequestElement);
-		
-		LOGGER.debug("FindPersonBySSNResponse JAXBElement: {}", 
-				(findPersonBySSNResponseElement != null ? ReflectionToStringBuilder.toString(findPersonBySSNResponseElement): null));
-
-		// Prepare the service response
-		final PersonInfoResponse personInfoResponse =
-				createPersonInfoResponse(findPersonBySSNResponseElement, personInfoRequest.getSsn());
-		LOGGER.debug("PersonInfoResponse: {}", 
-				ReflectionToStringBuilder.toString(personInfoResponse));
-
-		return personInfoResponse;
-	}
-	
-	/* (non-Javadoc)
-	 * @see gov.va.ascent.demo.service.api.DemoPersonService#findPersonByParticipantID
-	 *                 (gov.va.ascent.demo.partner.person.ws.client.transfer.PersonInfoRequest)
-	 * @Cacheable Annotation indicating that the result of invoking a method (or all methods in a class) can be cached.
-	 */
-	@Override
-	@Cacheable(value="demoPersonService", key="#personInfoRequest", unless="#result == null")
-	@HystrixCommand(
-				fallbackMethod = "getPersonInfoFallBack", 
-				commandKey = "GetPersonInfoByPIDCommand",
-				ignoreExceptions = {IllegalArgumentException.class})
-	public PersonInfoResponse findPersonByParticipantID(final PersonInfoRequest personInfoRequest) {
-		
-		// Check for valid input arguments and WS Client reference.
-		Defense.notNull(personWsClient, "Unable to proceed with Person Service request. The personWsClient must not be null.");
-		Defense.notNull(personInfoRequest.getParticipantID(), "Invalid argument, pid must not be null.");
-
-		// Prepare the WS request
-		final JAXBElement<FindPersonByPtcpntId> findPersonByPtcpntIdRequestElement = createFindPersonByPidRequest(personInfoRequest);
-
-		// Invoke the Person Web Service via the WS Client
-		final JAXBElement<FindPersonByPtcpntIdResponse> findPersonByPtcpntIdResponseElement =
-				personWsClient.getPersonInfoByPtcpntId(findPersonByPtcpntIdRequestElement);
-
-		// Prepare the service response
-		final PersonInfoResponse personInfoResponse =
-				createPersonInfoResponse(findPersonByPtcpntIdResponseElement, personInfoRequest.getParticipantID());
-		return personInfoResponse;
-
-	}
+        if (cacheManager.getCache("demoPersonService") != null && cacheManager.getCache("demoPersonService").get(personInfoRequest) != null) {
+          LOGGER.info("returning cached data for {}", personInfoRequest);
+          return cacheManager.getCache("demoPersonService").get(personInfoRequest, PersonInfoResponse.class);
+        } else {
+            // Prepare the WS request
+            final JAXBElement<FindPersonByPtcpntId> findPersonByPtcpntIdRequestElement = createFindPersonByPidRequest(personInfoRequest);
+    
+            // Invoke the Person Web Service via the WS Client
+            final JAXBElement<FindPersonByPtcpntIdResponse> findPersonByPtcpntIdResponseElement =
+                    personWsClient.getPersonInfoByPtcpntId(findPersonByPtcpntIdRequestElement);
+    
+            // Prepare the service response
+            final PersonInfoResponse personInfoResponse =
+                    createPersonInfoResponse(findPersonByPtcpntIdResponseElement, personInfoRequest.getParticipantID());
+            return personInfoResponse;
+        }
+    }
 	
 	/**
 	 * Hystrix Fallback Method Which is Triggered When there Is An Unexpected Exception
+	 * in getPersonInfo
 	 * @param personInfoRequest The request from the Java Service.
 	 * @return A JAXB element for the WS request
 	 */
-	@HystrixCommand(commandKey = "GetPersonInfoFallbackCommand")
-	public PersonInfoResponse getPersonInfoFallBack(PersonInfoRequest personInfoRequest) {
-	    if (cacheManager.getCache("demoPersonService") != null && cacheManager.getCache("demoPersonService").get(personInfoRequest) != null) {
-	    	LOGGER.info("getPersonInfoFallBack returning cached data for {}", personInfoRequest);
-	        return cacheManager.getCache("demoPersonService").get(personInfoRequest, PersonInfoResponse.class);
-	    } else {
-	    	LOGGER.error("getPersonInfoFallBack no cached data found raising exception for {}", personInfoRequest);
-	    	throw new AscentRuntimeException("No cached data found in the GetPersonInfoFallbackCommand. Raising an exception");
+	@HystrixCommand(commandKey = "GetPersonInfoFallBackCommand")
+	public PersonInfoResponse getPersonInfoFallBack(PersonInfoRequest personInfoRequest, Throwable throwable) {
+	  final PersonInfoResponse response = new PersonInfoResponse();
+	    if (throwable != null) {
+	      final String msg = throwable.getMessage();
+	      final List<Message> messages = new ArrayList<Message>();
+	      messages.add(newMessage(MessageSeverity.ERROR, "ERROR", msg));
+          response.setMessages(messages);
+          if (LOGGER.isDebugEnabled()) {
+              LOGGER.debug(msg);
+          }
+	      return response; 
+	    } 
+	    else {
+	      return response;  
 	    }
 	}
+	
+	/**
+     * Hystrix Fallback Method Which is Triggered When there Is An Unexpected Exception
+     * in findPersonByParticipantID method
+     * @param personInfoRequest The request from the Java Service.
+     * @return A JAXB element for the WS request
+     */
+    @HystrixCommand(commandKey = "FindPersonByParticipantIDFallBackCommand")
+    public PersonInfoResponse findPersonByParticipantIDFallBack(PersonInfoRequest personInfoRequest, Throwable throwable) {
+        final PersonInfoResponse response = new PersonInfoResponse();
+        if (throwable != null) {
+          final String msg = throwable.getMessage();
+          final List<Message> messages = new ArrayList<Message>();
+          messages.add(newMessage(MessageSeverity.FATAL, "FATAL", msg));
+          response.setMessages(messages);
+          if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(msg);
+          }
+          return response; 
+        } else {
+            LOGGER.error("findPersonByParticipantIDFallBack No Throwable Exception and No Cached Data. Just Raise Runtime Exception {}", personInfoRequest);
+            throw new AscentRuntimeException("There was a problem processing your request.");
+        }
+    }
+    
+    /**
+     * Helper method to create a Message object.
+     *
+     * @param severity the severity
+     * @param key the key
+     * @param text the text
+     * @return the message
+     */
+    private final Message newMessage(final MessageSeverity severity, final String key, final String text) {
+        final Message msg = new Message();
+        msg.setSeverity(severity);
+        msg.setKey(key);
+        msg.setText(text);
+        return msg;
+    }
 
 	
 	/**
