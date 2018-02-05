ec0a620ecdbb9b2cc5688a21216ef0576b681840
Abhijit Kulkarni <akulkarni@meetveracity.com>
2018-02-05 12:54:58 -0500

    Initial Baseline version for S3 and SQS POC

--- ascent-document-service/src/main/java/gov/va/ascent/document/service/rest/provider/DocumentServiceEndPoint.java
+++ ascent-document-service/src/main/java/gov/va/ascent/document/service/rest/provider/DocumentServiceEndPoint.java
@@ -1,36 +1,84 @@
 package gov.va.ascent.document.service.rest.provider;
 
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsOperations;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import gov.va.ascent.document.service.api.DocumentService;
 import gov.va.ascent.document.service.api.transfer.GetDocumentTypesResponse;
+import gov.va.ascent.framework.swagger.SwaggerResponseMessages;
+import gov.va.ascent.starter.aws.autoconfigure.s3.services.S3Services;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
-public class DocumentServiceEndPoint implements HealthIndicator {
+public class DocumentServiceEndPoint implements HealthIndicator, SwaggerResponseMessages {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(DocumentServiceEndPoint.class);
 	
 	@Autowired
 	@Qualifier("IMPL")
-	DocumentService documentService;	
+	DocumentService documentService;
+	
+	@Autowired
+	S3Services s3Services;
+	
+	@Autowired
+	JmsOperations jmsTemplate;
+	    
+	@Value("${ascent.s3.uploadfile}")
+	private String uploadFilePath;
+	    
+	@Value("${ascent.s3.key}")
+	private String downloadKey;
 
 	public static final String URL_PREFIX = "/document/v1";
 
 	@RequestMapping(value = URL_PREFIX + "/health", method = RequestMethod.GET)
 	public Health health() {
-		// TODO Auto-generated method stub
 		return Health.up().withDetail("Document Service REST Endpoint", "Document Service REST Provider Up and Running!").build();
+	}
+	
+	@RequestMapping(value = URL_PREFIX + "/uploadStaticFileDocument", method = RequestMethod.GET)
+    public void uploadStaticFileDocument() throws IOException {
+	  System.out.println("---------------- START UPLOAD FILE ----------------");
+      s3Services.uploadFile("ascent-s3-upload-file.txt", uploadFilePath);
+      //System.out.println("---------------- START DOWNLOAD FILE ----------------");
+      //s3Services.downloadFile(downloadKey);
+    }
+	
+	@PostMapping(value = URL_PREFIX + "/uploadDocument")
+    @ApiOperation(value = "Upload a Document",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> identify(final @RequestHeader HttpHeaders headers,
+                                      final @RequestBody MultipartFile documentOne
+    )
+    {
+	  return s3Services.uploadMultiPartSingle(documentOne);
+    }
+	
+	@PostMapping("/message")
+	public ResponseEntity<?> sendMessage(@RequestBody String message) {
+	   LOGGER.info("Sending message {}.", message);
+	   jmsTemplate.convertAndSend(message);
+	   return ResponseEntity.ok().build();
 	}
 	
 	@RequestMapping(value = URL_PREFIX + "/documentTypes", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
